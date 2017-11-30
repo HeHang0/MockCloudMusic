e7317bca3deeb8c0ddc5ed9ef2da0d19bc1ebb01
何航 <hehang724590957@qq.com>
2017-11-30 11:24:54 +0800

    定义MusicObservableCollection类

---
+++ MusicManager/MusicObservableCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCollection.MusicManager
+{
+    public class MusicObservableCollection: ObservableCollection<Music>
+    {
+        public new int Add(Music music)
+        {
+            int index = -1;
+            if (IsExist(music, out index))
+            {
+                return index;
+            }
+            else
+            {
+                base.Add(music);
+                index = this.Count - 1;
+            }
+            
+            return index;
+        }
+
+        private bool IsExist(Music music, out int index)
+        {
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this[i].Url == music.Url)
+                {
+                    index = i;
+                    return true;
+                }
+            }
+            index = -1;
+            return false;
+        }
+    }
+}
